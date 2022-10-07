9c975a97c94e357e54159229f7bbbb1fed96776a
Gabrielle Scott <scottgabrielle49@gmail.com>
2022-10-07 11:08:27 -0500

    Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,29 @@
+
+window.addEventListener("load",main)
+function main() {
+	var board = document.getElementById("board");
+	var squares = board.getElementsByTagName("div");
+	var counter=0;
+	console.log(counter);
+	for(let a = 0; a < squares.length; a++){
+      
+      squares[a].classList.add("square")
+      squares[a].addEventListener('click',function main(){
+
+      
+      });
+}
+
+
+	
+
+
+
+
+
+	}
+
+
+
+
+
